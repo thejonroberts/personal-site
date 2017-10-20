fafeebc0b83e07843fc1eeda7a9036aae048dfd8
thejonroberts <jon.roberts.8@gmail.com>
2017-10-19 21:46:32 -0500

    project hbs template, data json, layout touchups

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const $ = require('jquery');
+require('handlebars');
+
+const { projects } = require('../data/projects');
+let projectTemplate = require('../templates/project.hbs');
+
+let projectCards = projectTemplate({ projects });
+
+$('#projectList').append(projectCards);
