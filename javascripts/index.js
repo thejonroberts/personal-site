e49ffa1d2333abba6ff2cf57e24447f5ab3d1232
thejonroberts <jon.roberts.8@gmail.com>
2017-10-19 19:32:48 -0500

    box shadows, backgrounds for headings, section ps

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,1 @@
 'use strict';
-
-// require('../lib/node_modules/jquery/dist/jquery.js');
-// require('../lib/node_modules/hammerjs/hammer.js');
-// require('../lib/node_modules/materialize-css/dist/js/materialize.js');
-
-console.log('connected');
