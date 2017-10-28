2267bf39c43128614d495a35f7b29d132aff7a09
thejonroberts <jon.roberts.8@gmail.com>
2017-10-28 14:47:03 -0500

    fixed nav, collapsible with side nav

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const $ = require('jquery');
+// const $ = require('jquery');
 require('handlebars');
 
 const { projects } = require('../data/projects');
@@ -9,3 +9,12 @@ let projectTemplate = require('../templates/project.hbs');
 let projectCards = projectTemplate({ projects });
 
 $('#projectList').append(projectCards);
+
+// materialize initialization
+$(document).ready(function() {
+	$('.button-collapse').sideNav();
+});
+
+$('.side-nav').on('click', () => {
+	$('.button-collapse').sideNav('hide');
+});
