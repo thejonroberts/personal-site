cff8dc57bc52c9ef44bbc3c3b735d248130e7e8c
thejonroberts <jon.roberts.8@gmail.com>
2017-10-30 14:42:52 -0500

    basic tech grid - wip

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,12 +2,17 @@
 
 require('handlebars');
 
+// project list
 const { projects } = require('../data/projects');
 let projectTemplate = require('../templates/project.hbs');
-
 let projectCards = projectTemplate({ projects });
-
 $('#projectList').append(projectCards);
+
+// technology list
+const { technologies } = require('../data/technologies');
+let techTemplate = require('../templates/technologies.hbs');
+let techBadges = techTemplate({ technologies });
+$('#techList').append(techBadges);
 
 // materialize initialization
 $(document).ready(function() {
